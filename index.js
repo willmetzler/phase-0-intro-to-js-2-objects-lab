59eb7bba13305f6094ae94a55e05b1a5576414b6
Will Metzler <metzlerwill@gmail.com>
2024-02-07 15:30:10 -0500

    Added object functions

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+    name: "Will",
+    streetAddress: "85 Mountain Ave",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj};
+    delete obj.key;
+    return {newObj};
+    };
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
